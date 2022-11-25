c22d3e15799eb8974107200a96c064ae5165f282
jjcastillot <jjcastillot@gmail.com>
2022-11-25 17:31:22 -0500

    COMMIT1 - Started main script

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+# Main variables
+master_power = 'on'
+keep_working = True
+
+#Main display
+print("COFFEE MACHINE 3000")
+print('''
+    (  )   (   )  )
+     ) (   )  (  (
+     ( )  (    ) )
+     _____________
+    <_____________> ___
+    |             |/ _ \\
+    |               | | |
+    |               |_| |
+ ___|             |\___/
+/    \___________/    \\
+\_____________________/
+''')
+machine = CoffeeMaker()
+machine.report()
